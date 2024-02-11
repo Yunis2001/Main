83bf6d356a168b760f6d12bb630b7a9c093d55bf
eunice <eunicewanjiru208@gmail.com>
2024-02-11 13:27:13 +0300

    Capture user input for name using Scanner's nextLine() method

--- Main.java
+++ Main.java
@@ -4,7 +4,8 @@ public class Main
   public static void main(String []args){
     Scanner eunice = new Scanner(System.in);
     System.out.println("What is your favourite food?");
-    
+    String name = eunice.nextln(); 
+      System.out.println("Hello, my favourite food is, "+name);
   }
 
 }
